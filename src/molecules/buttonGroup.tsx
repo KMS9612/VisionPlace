07406a929ce57e3b9633a2610fa15ae44f48ce56
KMS9612 <kimdev9612@gmail.com>
2025-08-10 21:45:39 +0900

    feat: 상담하기 버튼을 누르면 contact Section으로 이동하도록 buttonGroup에 함수 추가

--- src/molecules/buttonGroup.tsx
+++ src/molecules/buttonGroup.tsx
@@ -17,11 +17,26 @@ const ButtonGroup = () => {
     { variant: "default", innerText: "상담하기" },
     { variant: "outline", innerText: "포트폴리오" },
   ];
+  const scrollToSection = (innerText: string) => {
+    if (innerText === "상담하기") {
+      const element = document.getElementById("contact");
+      if (element) {
+        element.scrollIntoView({
+          behavior: "smooth",
+          block: "start",
+        });
+      }
+    }
+  };
 
   return (
     <div id="button-wrap" className="flex gap-2">
       {BUTTON_ITEM.map((el) => (
-        <Button key={el.innerText} variant={el.variant}>
+        <Button
+          onClick={() => scrollToSection(el.innerText)}
+          key={el.innerText}
+          variant={el.variant}
+        >
           {el.innerText}
         </Button>
       ))}
